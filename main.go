9ec0061c4ddc3aa85a133a238f32f13ae85fdad8
helloWon <leeseongwon13@gmail.com>
2023-03-05 23:51:00 +0900

    3.1 slow urlChecker

--- main.go
+++ main.go
@@ -1,16 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
+var errRequestFailed = errors.New("Request failed")
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
 
 func main() {
-	foods := []string{"bread", "chicken"}
-	// nico := person{"nico", 12, foods}
-	nico := person{name: "nico", age: 12, favFood: foods}
-	fmt.Println(nico.name)
+	// var results = map[string]string{}
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	// fmt.Println(results)
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
 }
