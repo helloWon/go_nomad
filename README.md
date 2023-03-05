500c20cea1c57957f3dc2027b5f43fb57227083e
helloWon <leeseongwon13@gmail.com>
2023-03-05 21:00:20 +0900

    1.3-4 Functions

--- README.md
+++ README.md
@@ -16,4 +16,10 @@ Practice tutorial by nico: https://nomadcoders.co/go-for-beginners
 
 ## 1.3-4 Functions
 
--   go는 type language / type 명시 필요
+-   compiler가 build 할 수 있도록 args type 명시 필요
+-   여러 개의 return 값을 가질 수 있음
+-   여러 개의 args 받으려면 ...string
+-   'naked' return: return var 굳이 명시 필요 없음
+-   'defer': finally절 같이 func 종료 후 추가 작업 명시
+
+## 1.5 for, range
--- main.go
+++ main.go
@@ -1,15 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+// func multiply(a int, b int) int {
+func multiply(a, b int) int {
+	return a * b
+}
+
+// func lenAndUpper(name string) (int, string) {
+// 	return len(name), strings.ToUpper(name)
+// }
+
+func lenAndUpper(name string) (lenght int, uppercase string) {
+	defer fmt.Println("I'm done")
+	lenght = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
 
 func main()  {
-	// const name string = "sw"
-	var name string = "sw"
-	name = "lynn"
-	fmt.Println(name)
+	// 곱셈
+	fmt.Println(multiply(2,2))
 
-	name2 := "sw"
-	name2 = "lynn"
-	fmt.Println(name2)
+	// 값 여러 개 return
+	totalLenght, upperName := lenAndUpper("sw")
+	fmt.Println(totalLenght, upperName)
 
+	// args 여러 개
+	repeatMe("nico", "sw", "lynn")
 }
