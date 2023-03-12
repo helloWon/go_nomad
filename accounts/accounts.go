4db03cd1eda51b862407826ea67ffaa7b4041814
helloWon <leeseongwon13@gmail.com>
2023-03-12 18:32:37 +0900

    2.2-3 Methods & err

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,13 +1,35 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string
 	balance int
 }
 
+var errNoMoney = errors.New("Can't withdraw")
+
 // NewAccount creates Account
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit: Method == receiver
+func (a *Account) Deposit(amount int) { // 실제 account를 받아오기 위해 *Account로 받아옴
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"hello/accounts"
+	"log"
 )
 
 func main() {
 	account := accounts.NewAccount("nico")
-	fmt.Println(account)
+
+	account.Deposit(10)
+	fmt.Println(account.Balance())
+
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(account.Balance())
 }
