32697a04ca3f07ac90e0f151d7e172130ff92c40
helloWon <leeseongwon13@gmail.com>
2023-03-14 01:32:44 +0900

    4.2-3 extractJob

--- main.go
+++ main.go
@@ -5,23 +5,57 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id       string
+	location string
+	title    string
+	// salary   string
+	// summary  string
+}
+
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=python"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractJobs := getPage(i)
+		jobs = append(jobs, extractJobs...) // list.extend()
 	}
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".item_recruit")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("value")
+	title := cleanString(card.Find("h2>a>span").Text())
+	location := cleanString(card.Find(".job_condition>span>a").Text())
+	fmt.Println(id, title, location)
+	return extractedJob{id: id, title: title, location: location}
 }
 
 func getPages() int {
@@ -39,7 +73,7 @@ func getPages() int {
 		// fmt.Println(s.Html())
 		pages = s.Find("a").Length()
 	})
-	return pages
+	return pages / 5
 }
 
 func checkErr(err error) {
@@ -52,4 +86,8 @@ func checkCode(res *http.Response) {
 	if res.StatusCode != 200 {
 		log.Fatalln("Request failed with Status:", res.StatusCode)
 	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
